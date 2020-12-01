1bf6b5ba482faff7c43da48fb15f8714cfc70e4f
Marcin Olichwiruk <marcin@opensoftware.pl>
2020-12-01 18:39:19 +0100

    Solve second part ⭐️

--- day1/main.test.js
+++ day1/main.test.js
@@ -1,8 +1,8 @@
 const calculate = require('./main')
 
-test('test', () => {
+test('test 3 numbers', () => {
   const filepath = `${__dirname}/input_test.txt`
-  const result = 514579
+  const result = 241861950
 
   expect(calculate(filepath)).toBe(result);
 })
