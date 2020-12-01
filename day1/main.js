d455e416fd3581f88a3c118f84ebec51de861e46
Marcin Olichwiruk <marcin@opensoftware.pl>
2020-12-01 13:11:25 +0100

    Solve first part ⭐️

--- day1/main.js
+++ day1/main.js
@@ -1,4 +1,21 @@
+const fs = require('fs')
+
 function calculate(filepath) {
-  return 0;
+  const input = fs.readFileSync(filepath, 'utf8')
+  let n1, n2
+  const numbers = input.split('\n').map(el => Number(el))
+
+  for (var i = 0; i < numbers.length; i++) {
+    const num = numbers[i]
+    const difference = 2020 - num
+    const isPair = numbers.includes(difference)
+    if (isPair) {
+      n1 = num
+      n2 = difference
+      break;
+    }
+  }
+
+  return n1 * n2;
 }
 module.exports = calculate;
